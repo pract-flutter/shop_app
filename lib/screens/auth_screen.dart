23704c5616347fdc9c74d240e5fcca2b18140cb3
HugoNguyen <nguyenquoctuan61089@gmail.com>
2021-02-22 23:59:37 +0700

    more transition

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -105,27 +105,37 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  // AnimationController _animationController;
-  // Animation<Size> _heightAnimation;
+  AnimationController _animationController;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    // _animationController = AnimationController(
-    //     vsync: this,
-    //     duration: Duration(
-    //       milliseconds: 300,
-    //     ));
-    // _heightAnimation = Tween<Size>(
-    //   begin: Size(double.infinity, 260),
-    //   end: Size(double.infinity, 320),
-    // ).animate(
-    //   CurvedAnimation(
-    //     parent: _animationController,
-    //     curve: Curves.fastOutSlowIn,
-    //   ),
-    // );
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, -1.5),
+      end: Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
 
     // _heightAnimation.addListener(() => setState(() {}));
   }
@@ -205,12 +215,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      // _animationController.forward();
+      _animationController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      // _animationController.reverse();
+      _animationController.reverse();
     }
   }
 
@@ -260,19 +270,33 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                          }
-                        : null,
+                AnimatedContainer(
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
                   ),
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
