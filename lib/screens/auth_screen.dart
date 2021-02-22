2cfe326df118f9fe8592150bd73938e3a59ca94b
HugoNguyen <nguyenquoctuan61089@gmail.com>
2021-02-22 23:45:23 +0700

    use AnimatedContainer

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -105,27 +105,27 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  AnimationController _animationController;
-  Animation<Size> _heightAnimation;
+  // AnimationController _animationController;
+  // Animation<Size> _heightAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    _animationController = AnimationController(
-        vsync: this,
-        duration: Duration(
-          milliseconds: 300,
-        ));
-    _heightAnimation = Tween<Size>(
-      begin: Size(double.infinity, 260),
-      end: Size(double.infinity, 320),
-    ).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.fastOutSlowIn,
-      ),
-    );
+    // _animationController = AnimationController(
+    //     vsync: this,
+    //     duration: Duration(
+    //       milliseconds: 300,
+    //     ));
+    // _heightAnimation = Tween<Size>(
+    //   begin: Size(double.infinity, 260),
+    //   end: Size(double.infinity, 320),
+    // ).animate(
+    //   CurvedAnimation(
+    //     parent: _animationController,
+    //     curve: Curves.fastOutSlowIn,
+    //   ),
+    // );
 
     // _heightAnimation.addListener(() => setState(() {}));
   }
@@ -134,7 +134,7 @@ class _AuthCardState extends State<AuthCard>
   void dispose() {
     super.dispose();
 
-    _animationController.dispose();
+    // _animationController.dispose();
   }
 
   void _showErrorDialog(String message) {
@@ -205,12 +205,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      _animationController.forward();
+      // _animationController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _animationController.reverse();
+      // _animationController.reverse();
     }
   }
 
@@ -222,16 +222,14 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
-        ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
